bac21b9879bf67885489778f8bda2f34e316550a
Kimi A <kimianderssonmusic@gmail.com>
2025-09-05 19:16:32 +0200

    What2.0

--- portolio-site/src/app/page.tsx
+++ portolio-site/src/app/page.tsx
@@ -1,10 +1,10 @@
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Experience from "@/components/Experience";
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Experience from "../components/Experience";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 import Image from "next/image";
 
 export default function Home() {
